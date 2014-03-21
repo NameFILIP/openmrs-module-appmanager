6ec31d25108f0350df61253807aa5c821317c1b0
Filip Spiridonov <filip.spiridonov@gmail.com>
2014-03-21 16:17:39 +0200

    Add very simple uploading app functionality

--- omod/src/main/java/org/openmrs/module/appmanager/web/controller/AppManagerManageController.java
+++ omod/src/main/java/org/openmrs/module/appmanager/web/controller/AppManagerManageController.java
@@ -13,24 +13,78 @@
  */
 package org.openmrs.module.appmanager.web.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ant.compress.taskdefs.Unzip;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
+import org.openmrs.module.ModuleUtil;
+import org.openmrs.web.WebUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 /**
  * The main controller.
  */
 @Controller
-public class  AppManagerManageController {
+public class AppManagerManageController {
+	
+	@Autowired
+	ServletContext servletContext;
 	
 	protected final Log log = LogFactory.getLog(getClass());
 	
 	@RequestMapping(value = "/module/appmanager/manage", method = RequestMethod.GET)
 	public void manage(ModelMap model) {
-		model.addAttribute("user", Context.getAuthenticatedUser());
+		String realPath = servletContext.getRealPath("");
+		
+		File appFolder = new File(realPath + "apps/");
+		String[] appList = appFolder.list();
+		
+		model.addAttribute("appList", appList);
+	}
+	
+	@RequestMapping(value = "/module/appmanager/manage", method = RequestMethod.POST)
+	public void upload(ModelMap model, HttpServletRequest request) {
+		
+		String realPath = servletContext.getRealPath("");
+		
+		InputStream inputStream = null;
+		try {
+			if (request instanceof MultipartHttpServletRequest) {
+				
+				MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+				MultipartFile multipartModuleFile = multipartRequest.getFile("appFile");
+				if (multipartModuleFile != null && !multipartModuleFile.isEmpty()) {
+					String filename = WebUtil.stripFilename(multipartModuleFile.getOriginalFilename());
+					
+					inputStream = multipartModuleFile.getInputStream();
+					File appFile = ModuleUtil.insertModuleFile(inputStream, filename);
+					String dest = realPath + "apps/" + filename.substring(0, filename.lastIndexOf('.'));
+					Unzip unzip = new Unzip();
+					unzip.setSrc(appFile);
+					unzip.setDest(new File(dest));
+					unzip.execute();
+				}
+			}
+		}
+		catch (IOException e) {
+			log.error("Error generated", e);
+		}
+		finally {
+			IOUtils.closeQuietly(inputStream);
+		}
+		
 	}
 }
--- omod/src/main/webapp/manage.jsp
+++ omod/src/main/webapp/manage.jsp
@@ -3,6 +3,21 @@
 
 <%@ include file="template/localHeader.jsp"%>
 
-<p>Hello ${user.systemId}!</p>
+<b class="boxHeader"><openmrs:message code="App.add" /></b>
+<div class="box">
+	<form id="appAddForm" action="" method="post"
+		enctype="multipart/form-data">
+		<input type="file" name="appFile" size="40" /> 
+		<input type="hidden" name="action" value="upload" /> 
+		<input type="submit" value='<openmrs:message code="App.upload"/>' />
+	</form>
+</div>
+
+<p>Installed apps:</p>
+<ul>
+	<c:forEach var="app" items="${appList}">
+		<li>${app}</li>
+	</c:forEach>
+</ul>
 
 <%@ include file="/WEB-INF/template/footer.jsp"%>
